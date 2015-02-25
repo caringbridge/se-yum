38835b8a7cd493ca95b5f971c975b372813b638f
svanzee4cb <svanzee@Van-Zees-MacBook-Pro.local>
2015-02-25 13:42:51 -0600

    commented out mirrorlist to resolve proxy miss issues

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,8 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+execute "disable mirrorlist for proxy" do
+  command "sed -e 's/^#baseurl/baseurl/g' -e 's/^mirrorlist/#mirrorlist/g'  -i /etc/yum.repos.d/*.repo"
+  only_if do ::File.exists?('/etc/profile.d/vagrant.sh') end
+end
