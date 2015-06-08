4cd4d2178c8487e4230bdb6f7990e3814aa7a75b
Stephen Van Zee <svanzee@caringbridge.org>
2015-06-08 11:01:06 -0500

    added include yum recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,8 +6,9 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe 'yum'
 
 execute "disable mirrorlist for proxy" do
-  command "sed -e 's/^#baseurl/baseurl/g' -e 's/^mirrorlist/#mirrorlist/g'  -i /etc/yum.repos.d/*.repo"
+  command "sed -e 's/^#baseurl/baseurl/g' -e 's/^mirrorlist/#mirrorlist/g'  -i /etc/yum.repos.d/CentOS-*.repo"
   only_if do ::File.exists?('/etc/profile.d/vagrant.sh') end
 end
